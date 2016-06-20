edd09c14b559d87aee9d194e038ad87ef9674a49
NablaT <remi.pourtier@laposte.net>
2016-06-21 00:11:15 +0200

    Almost nothing

--- src/app/components/mocks/mock-conversation.ts
+++ src/app/components/mocks/mock-conversation.ts
@@ -10,6 +10,6 @@ export var MESSAGES: MessageModel[] = [
     {message: 'How are you pouet?', from: 'server', to:"client", contactID:"12"},
     {message: 'Im pointing fine and you?', from: 'client', to:"server", contactID:"12"},
     {message: 'Bijour', from: 'server', to:"client", contactID:"13"},
-    {message: 'Bijour', from: 'server', to:"client", contactID:"14"},
+    {message: 'WESH TA VU CA MARCHE :D', from: 'server', to:"client", contactID:"14"},
     {message: 'Tu as ma tune ?', from: 'server', to:"client", contactID:"15"},
 ];
