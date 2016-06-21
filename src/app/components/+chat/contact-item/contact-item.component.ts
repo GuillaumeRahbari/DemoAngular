f3ac432fb23dc08c276147cbe32df08b0a630cd7
NablaT <remi.pourtier@gmail.com>
2016-06-21 11:44:16 +0200

    Fixing issue after a pull

--- src/app/components/+chat/contact-item/contact-item.component.ts
+++ src/app/components/+chat/contact-item/contact-item.component.ts
@@ -4,6 +4,8 @@
 
 import {Component, Output, Input, EventEmitter} from '@angular/core';
 import {ContactModel} from "../../models/contact-model/contact.model";
+import {MessageModel} from "../../models/message-model/message.model";
+import {ManageChatService} from "../../../shared/services/src/manage-chat.service";
 
 @Component({
     selector: 'contact-item',
