4fc8bae96aa0b2235d5ab633cd00166e70648957
NablaT <remi.pourtier@gmail.com>
2016-06-22 15:22:55 +0200

    updating conversation

--- src/app/components/mocks/mock-conversation.ts
+++ src/app/components/mocks/mock-conversation.ts
@@ -5,11 +5,11 @@ export var MESSAGES: MessageModel[] = [
     {message: "Hey", from: "server", to:"client", contactID:"11"},
     {message: "How are you doing?", from: "server", to:"client", contactID:"11"},
     {message: "Fine and you?", from: "client", to:"server", contactID:"11"},
-    {message: "Fine', from: 'server", to:"client", contactID:"11"},
-    {message: "Pouet', from: 'server", to:"client", contactID:"12"},
-    {message: "How are you pouet?", from: 'server', to:"client", contactID:"12"},
-    {message: "Im pointing fine and you?", from: "client", to:"server", contactID:"12"},
-    {message: "Bijour", from: 'server', to:"client", contactID:"13"},
-    {message: "WESH TA VU CA MARCHE :D", from: "server", to:"client", contactID:"14"},
-    {message: "Tu as ma tune ?", from: "server", to:"client", contactID:"15"}
+    {message: "Fine", from: "server", to:"client", contactID:"11"},
+    {message: "Hey", from: "server", to:"client", contactID:"12"},
+    {message: "What's up?", from: 'server', to:"client", contactID:"12"},
+    {message: "Nothing", from: "client", to:"server", contactID:"12"},
+    {message: "Don't say a word about GOT", from: 'server', to:"client", contactID:"13"},
+    {message: ":D :D", from: "server", to:"client", contactID:"14"},
+    {message: "When will you come ?", from: "server", to:"client", contactID:"15"}
 ];
